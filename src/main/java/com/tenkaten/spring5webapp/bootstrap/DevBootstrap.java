2a1c499b204912d019e05f72b2fbbea8506a69d0
Koen ten Katen <koentk84@gmail.com>
2017-08-19 20:21:38 +0200

    add publisher entity

--- src/main/java/com/tenkaten/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/tenkaten/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.tenkaten.spring5webapp.bootstrap;
 
 import com.tenkaten.spring5webapp.model.Author;
 import com.tenkaten.spring5webapp.model.Book;
+import com.tenkaten.spring5webapp.model.Publisher;
 import com.tenkaten.spring5webapp.respositories.AuthorRepository;
 import com.tenkaten.spring5webapp.respositories.BookRepository;
+import com.tenkaten.spring5webapp.respositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,24 +15,32 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher publisher1 = new Publisher("Harper Collins", "New York");
+        Book ddd = new Book("Domain Driven Design", "1234", publisher1);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher1);
+        publisherRepository.save(publisher1);
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEjb = new Book("J2EE Development without EJB", "2344", "Worx");
+        Publisher publisher2 = new Publisher("Worx", "Amsterdam");
+        Book noEjb = new Book("J2EE Development without EJB", "2344", publisher2);
+        noEjb.setPublisher(publisher2);
         rod.getBooks().add(noEjb);
         noEjb.getAuthors().add(rod);
+        publisherRepository.save(publisher2);
         authorRepository.save(rod);
         bookRepository.save(noEjb);
     }
---
+++ src/main/java/com/tenkaten/spring5webapp/respositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.tenkaten.spring5webapp.respositories;
+
+import com.tenkaten.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
